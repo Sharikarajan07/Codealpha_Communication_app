66cc464d7d0ca1d2d53960410375cbf82c6eb194
SHARIKA RAJAN <151808048+Sharikarajan07@users.noreply.github.com>
2025-08-22 23:38:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app)
 
 // CORS configuration - FIXED
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173","https://codealpha-communication-app.vercel.app","https://codealpha-communication.vercel.app"],
+  origin: ["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173","https://codealpha-communication-app.vercel.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
